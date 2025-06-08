7631e3d7b22ffe9e9a57d98643eec28cb98c7239
junmojin <o3o060@naver.com>
2025-06-08 16:18:41 +0900

    회원 서비스 (회원가입, 회원조회)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
+    //MemberServiceImpl : 회원 서비스 구현체(회원가입, 회원조회 기능)
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
