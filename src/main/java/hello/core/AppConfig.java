8f48cbea9982e98165988ec33840895490aec9bb
junmojin <o3o060@naver.com>
2025-06-10 23:42:41 +0900

    스프링으로 전환 (@Bean)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,21 +8,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // AppConfig : 애플리케이션의 전체 동작 방식을 구성(config)하기 위해 '구현 객체를 생성'하고 '연결'하는 책임
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+       // AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
        //MemberService memberService = new MemberServiceImpl();
+
+        //AppConfig의 환경설정 파일을 @Bean이라 적힌 곳에 스프링 빈 컨테이너에 넣어서 다 관리한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //메서드 이름인 memberService 객체를 찾기 위한것, 두번째는 타입적는거임
+
         Member member = new Member(1L,"memberA", Grade.VIP);
         memberService.join(member);
 
